cee78e11a0a70c2502b6d4782850358efd0595d2
neshadsfz <42376226+neshadsfz@users.noreply.github.com>
2019-10-10 16:31:25 +0530

    Add files via upload

---
+++ String Size.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  string 1
+//
+//  Created by APPLE on 2/18/19.
+//  Copyright © 2019 APPLE. All rights reserved.
+//
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    string s1,s2("Name of      student");
+    cout<<s2<<endl;
+    int x=s2.size();
+    cout<<x;
+}
