31edcc61ca56d11ff18115ed11a5629f423d87a1
405Abhishek <56436286+405Abhishek@users.noreply.github.com>
2019-10-11 16:33:52 +0530

    Add files via upload

---
+++ area rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	cout<<"enter the length of rectangle \n";
+	cin>>b;
+	cout<<"enter the breadth of rectangle \n";
+    cin>>a;
+    c=a*b;
+    cout<<"the area of rectangle is "<<c;
+    return 0;
+}
