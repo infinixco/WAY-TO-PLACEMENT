54c66e028a0beeb4c54a141cd5003274a11c99c6
piyushsinha9 <49586397+piyushsinha9@users.noreply.github.com>
2019-10-23 12:21:09 +0530

    piyush's test-2

---
+++ balanced.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t,sum=0,count=0;
+cin>>t;
+int arr[t];
+int arr1[t];
+for(int i=0;i<t;i++){
+	cin>>arr[i];
+}
+for(int i=0;i<t;i++){
+
